582f387e7daf347f82bcc2c26e15086726faab89
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2016-04-18 20:08:24 +0200

    Camera is now responsible for ray generation

--- src/camera.jl
+++ src/camera.jl
@@ -1,6 +1,9 @@
-abstract Camera
+abstract Camera{T}
 
-type SimpleCamera{T<:AbstractFloat} <: Camera
+import Base.eltype
+eltype{T}(c::Camera{T}) = T
+
+type SimpleCamera{T<:AbstractFloat} <: Camera{T}
     pos::Point{3,T}
     up::Vec{3,T}
     fwd::Vec{3,T}
@@ -10,6 +13,12 @@ type SimpleCamera{T<:AbstractFloat} <: Camera
     d::T
     w_px::Integer
     h_px::Integer
+    dx::T
+    dy::T
+    x1::Vec{3,T}
+    x2::Vec{3,T}
+    y1::Vec{3,T}
+    y2::Vec{3,T}
 end
 function SimpleCamera{T<:AbstractFloat}(pos::Point{3,T},
                                         up::Vec{3,T},
@@ -17,9 +26,33 @@ function SimpleCamera{T<:AbstractFloat}(pos::Point{3,T},
                                         w::T, h::T, d::T,
                                         w_px, h_px)
     up,fwd = normalize(up), normalize(fwd)
-    SimpleCamera(pos, up, fwd, cross(up,fwd),
+    right = cross(up,fwd)
+
+    dx = 1.0/(w_px - 1)
+    dy = 1.0/(h_px - 1)
+
+    x1 = d*fwd - 0.5w*right
+    x2 = d*fwd + 0.5w*right
+    y1 = d*fwd + 0.5h*up
+    y2 = d*fwd - 0.5h*up
+
+    SimpleCamera(pos, up, fwd, right,
                  w, h, d,
-                 w_px, h_px)
+                 w_px, h_px,
+                 dx, dy,
+                 x1, x2, y1, y2)
 end
 
-export SimpleCamera
+import Images.width, Images.height
+width(cam::SimpleCamera) = cam.w_px
+height(cam::SimpleCamera) = cam.h_px
+
+import Base.getindex
+function getindex{T<:Real}(cam::SimpleCamera, i::T, j::T)
+    t_x = (i-1)*cam.dx
+    t_y = (j-1)*cam.dy
+    dir = t_x * cam.x1 + (1.0-t_x) * cam.x2 + t_y * cam.y1 + (1.0-t_y) * cam.y2
+    Ray{eltype(cam.pos)}(cam.pos, dir)
+end
+
+export Camera, SimpleCamera, getindex, width, height
--- src/render.jl
+++ src/render.jl
@@ -2,23 +2,13 @@ using FixedSizeArrays
 using Images, ColorTypes
 using ProgressMeter
 
-function render{T<:AbstractFloat}(f::Function, cam::SimpleCamera{T})
-    img = Image(zeros(RGB{T}, (cam.w_px, cam.h_px)))
+function render{C<:Camera}(f::Function, cam::C)
+    w,h = width(cam),height(cam)
+    img = Image(zeros(RGB{eltype(cam)}, (w,h)))
 
-    dx = 1.0/(cam.w_px - 1)
-    dy = 1.0/(cam.h_px - 1)
-
-    x1 = cam.d*cam.fwd - 0.5cam.w*cam.right
-    x2 = cam.d*cam.fwd + 0.5cam.w*cam.right
-    y1 = cam.d*cam.fwd + 0.5cam.h*cam.up
-    y2 = cam.d*cam.fwd - 0.5cam.h*cam.up
-
-    @showprogress "Rendering: " for i = 1:cam.w_px
-        for j = 1:cam.h_px
-            t_x = (i-1)*dx
-            t_y = (j-1)*dy
-            dir = t_x * x1 + (1.0-t_x) * x2 + t_y * y1 + (1.0-t_y) * y2
-            img[i,j] = f(Ray{T}(cam.pos, dir))
+    @showprogress "Rendering: " for i = 1:w
+        for j = 1:h
+            img[i,j] = f(cam[i,j])
         end
     end
 
