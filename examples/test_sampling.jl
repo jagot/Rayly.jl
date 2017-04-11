fe3ca87d7463391c5d9d370c94dfae7e74d70ae7
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2017-04-11 19:23:50 +0200

    Clarified sampling example a little bit

--- examples/test_sampling.jl
+++ examples/test_sampling.jl
@@ -1,9 +1,6 @@
 using Rayly
-
-using PyCall
-pygui(:qt)
 using PyPlot
-matplotlib[:rcdefaults]()
+MultipleLocator = matplotlib[:ticker][:MultipleLocator]
 
 function test_sampling(s)
     i,j = 10.,9.
@@ -11,6 +8,12 @@ function test_sampling(s)
     x,y = zip(samples...)
     plot(x,y, ".")
     axis([i-0.5,i+0.5,j-0.5,j+0.5])
+    for ax in [:xaxis,:yaxis]
+        gca()[ax][:set_major_locator](MultipleLocator(1))
+        gca()[ax][:set_minor_locator](MultipleLocator(0.1))
+    end
+    grid(which="major", linewidth=1.0)
+    grid(which="minor", linewidth=0.5)
 end
 
 figure(1,figsize=(8,4))
@@ -19,7 +22,7 @@ subplot(121)
 test_sampling(SingleSampler())
 title("Single sample")
 subplot(122)
-test_sampling(JitteredSampler(25))
+test_sampling(JitteredSampler(100))
 title("Jittered sampling")
 tight_layout()
-savefig("sampling.png")
+savefig("sampling.png", dpi=200)
