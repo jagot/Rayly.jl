edd9c15637413ec6e85e8def00bbb0a42009d82f
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-01-05 12:32:23 +0100

    Type stability of AABBs

--- src/aabb.jl
+++ src/aabb.jl
@@ -22,9 +22,9 @@ center(a::AABB) = (a.pmin+a.pmax)/2
 # http://psgraphics.blogspot.se/2016/02/new-simple-ray-box-test-from-andrew.html
 function Base.intersect(ray::Ray{T}, ab::AABB{T}) where T
     for a = 1:3
-        invD = 1.0/ray.dir[a]
-        t0 = (ab.pmin[a] - ray.pos[a])* invD
-        t1 = (ab.pmax[a] - ray.pos[a])* invD
+        invD = one(T)/ray.dir[a]
+        t0 = (ab.pmin[a] - ray.pos[a]) * invD
+        t1 = (ab.pmax[a] - ray.pos[a]) * invD
         if invD < 0
             t0,t1 = t1,t0
         end
